d8660b72ec3d1ea5bf6d6d538417e52510200a3e
Matthew Perry <muffinman616@gmail.com>
2013-01-21 11:48:00 -0500

    Clean up command line arg parsing

--- lib/youtube-dl.rb
+++ lib/youtube-dl.rb
@@ -2,6 +2,14 @@ require "youtube-dl/version"
 require "youtube-dl/application"
 
 module Youtube
+  HEADERS = {
+    'User-Agent' => 'Mozilla/5.0 (X11; Linux x86_64; rv:10.0) Gecko/20100101 Firefox/10.0',
+    'Accept-Charset' => 'ISO-8859-1,utf-8;q=0.7,*;q=0.7',
+    'Accept' => 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Encoding' => 'gzip, deflate',
+    'Accept-Language' => 'en-us,en;q=0.5',
+  }
+
   def self.application
     @application ||= Youtube::Application.new
   end
